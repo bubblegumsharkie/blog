812c423d1707e1a70973177aca28d50b08506832
Alex <kozlov.alex.dev@gmail.com>
2021-02-19 20:55:20 +0300

    New features: 1. You can now edit posts! 2. Minor updates

--- src/main/java/com/sharkie/demo/controllers/BlogController.java
+++ src/main/java/com/sharkie/demo/controllers/BlogController.java
@@ -6,8 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Optional;
 
 @Controller
 public class BlogController {
@@ -31,6 +35,40 @@ public class BlogController {
     @PostMapping("/blog/add")
     public String blogAddPost(@RequestParam String title, String short_text, String full_text, Model model) {
         Post post = new Post(title, short_text, full_text);
+        postRepository.save(post);
+        return "redirect:/blog";
+    }
+
+   @GetMapping("/blog/{id}")
+   public String blogDetails(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> posts = new ArrayList<>();
+        post.ifPresent(posts::add);
+        model.addAttribute("post", posts);
+        return "blog-details";
+   }
+
+    @GetMapping("/blog/{id}/edit")
+    public String blogPostEdit(@PathVariable(value = "id") long id, Model model) {
+        if (!postRepository.existsById(id)) {
+            return "redirect:/blog";
+        }
+        Optional<Post> post = postRepository.findById(id);
+        ArrayList<Post> posts = new ArrayList<>();
+        post.ifPresent(posts::add);
+        model.addAttribute("post", posts);
+        return "blog-edit";
+    }
+
+    @PostMapping("/blog/{id}/edit")
+    public String blogPostUpdate(@PathVariable(value = "id") long id, @RequestParam String title, String short_text, String full_text, Model model) {
+        Post post = postRepository.findById(id).orElseThrow();
+        post.setTitle(title);
+        post.setShort_text(short_text);
+        post.setFull_text(full_text);
         postRepository.save(post);
         return "redirect:/blog";
     }
